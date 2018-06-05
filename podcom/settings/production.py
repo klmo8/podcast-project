e16de5866a630905902a0f55669b3b879403536e
Kyle <kyelmo8@gmail.com>
2018-06-05 06:35:05 -0700

    updated requirements.txt

--- podcom/settings/production.py
+++ podcom/settings/production.py
@@ -166,3 +166,5 @@ CSRF_COOKIE_SECURE              = True
 SECURE_HSTS_INCLUDE_SUBDOMAINS  = True
 SECURE_HSTS_SECONDS             = 1000000
 SECURE_FRAME_DENY               = True
+
+django_heroku.settings(locals()
